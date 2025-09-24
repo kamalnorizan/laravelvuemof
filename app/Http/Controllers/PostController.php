2a5ebfe0788f1204efb6720f3f46ef065c8e0fee
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-09-24 11:57:08 +0800

    posts interface

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Posts/Index', ['posts' => Post::all()]);
+    }
 }
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <h1>Posts</h1>
+    <div class="border p-4 mb-2 rounded" v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <h2>{{ post.body }}</h2>
+    </div>
+</template>
+<script setup>
+defineProps({
+    posts:Array
+})
+</script>
