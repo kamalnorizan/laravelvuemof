9b7404d93b805a19b9db53226c132bc4986cd9c3
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-09-24 11:33:09 +0800

    post, comment and users

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'author' => rand(1, User::count()), // Assuming you have users with IDs from 1 to 10
+        ];
+    }
+}
